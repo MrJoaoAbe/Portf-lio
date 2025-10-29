e75478def72a0016d8471f7899533326bbe53833
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-10-29 19:49:11 -0300

    Primeiro Commit

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, createBrowserRouter } from "react-router-dom";
+import { Layout } from '../Layout';
+import { Home } from "../pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            { index: true, element: <Home /> }
+        ]
+    }
+])
