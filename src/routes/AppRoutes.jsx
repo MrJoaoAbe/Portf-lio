46160cd2f317ad59f001d039b4daf17ff3c732c7
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-10-29 19:55:32 -0300

    add components

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, createBrowserRouter } from "react-router-dom";
 import { Layout } from '../Layout';
 import { Home } from "../pages/Home";
+import { Contato } from "../pages/Contato";
+import { Projetos } from "../pages/Projetos";
+import { SobreMim } from "../pages/SobreMim";
+
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         children: [
-            { index: true, element: <Home /> }
+            { index: true, element: <Home /> },
+            { path: "contato", element: <Contato /> },
+            { path: "projetos", element: <Projetos /> },
+            { path: "sobremim", element: <SobreMim /> }
         ]
     }
 ])
