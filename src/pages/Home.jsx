ad767e113d04c68caac8b6a18ef89f851491d46c
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-10-29 21:57:03 -0300

    home page add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 function Home() {
     return (
-        <div>Hello World</div>
+        <div className="min-h-screen bg-linear-to-t from-[#8C0303] to-[#ac0101] text-white">
+            <div className="grid grid-cols-2">
+                <div className="flex justify-center mt-70 text-8xl mb-10">
+                    João Abe
+                </div>
+            </div>
+
+            <div className="ml-50 p-5 text-xl wrap-break-word w-220 border-l-4">
+                <p>Me chamo João Victor de Souza Abe e sou fanático por tecnologia! Clique em SOBRE MIM para saber mais da minha história.</p>
+            </div>
+        </div>
     )
 }
 
