47147fdd7bf7b1b350ea9e42eae7be194fa44752
MrJoaoAbe <joaovictorsouzaabe@gmail.com>
2025-10-30 20:41:59 -0300

    add icons

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faEnvelope, faFile, faComment } from "@fortawesome/free-solid-svg-icons";
 
 function Contato() {
     return (
@@ -14,9 +14,20 @@ function Contato() {
                         <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff", }} className="absolute bottom-4 left-140" />
                     </div>
 
-                    <input className="border-b-2 w-150 rounded-sm outline-none pl-5" placeholder="Nome@email.com" type="email" name="email" />
-                    <input className="border-b-2 w-150 rounded-sm outline-none pl-5" placeholder="Assunto" type="text" name="assunto" />
-                    <textarea className="border-2 w-150 h-50 rounded-sm outline-none p-5" placeholder="Mensagem" type="" name="Mensagem" />
+                    <div className="relative">
+                        <input className="border-b-2 w-150 rounded-sm outline-none pl-5" placeholder="Nome@email.com" type="email" name="email" />
+                        <FontAwesomeIcon icon={faEnvelope} style={{ color: "#ffffff", }} className="absolute bottom-4 left-140" />
+                    </div>
+
+                    <div className="relative">
+                        <input className="border-b-2 w-150 rounded-sm outline-none pl-5" placeholder="Assunto" type="text" name="assunto" />
+                        <FontAwesomeIcon icon={faFile} style={{ color: "#ffffff", }} className="absolute bottom-4 left-140" />
+                    </div>
+
+                    <div className="relative">
+                        <textarea className="border-2 w-150 h-50 rounded-sm outline-none p-5" placeholder="Mensagem" type="" name="Mensagem" />
+                        <FontAwesomeIcon icon={faComment} style={{ color: "#ffffff", }} className="absolute top-5 left-140" />
+                    </div>
                 </form>
             </div>
         </div>
